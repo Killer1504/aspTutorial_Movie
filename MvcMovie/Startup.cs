79ef3de765c9edc1a4a42b46847e2882d063a20b
Killer1504 <thanhhungbkhn@gmail.com>
2021-12-15 15:43:02 +0700

    add data

--- MvcMovie/Startup.cs
+++ MvcMovie/Startup.cs
@@ -7,6 +7,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MvcMovie.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace MvcMovie
 {
@@ -23,6 +25,8 @@ namespace MvcMovie
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<MvcMovieContext>(options =>
+    options.UseSqlServer(Configuration.GetConnectionString("MvcMovieContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,5 +53,6 @@ namespace MvcMovie
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
+        
     }
 }
